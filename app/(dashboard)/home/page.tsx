9fc78d3263559d8cfc553ffe2b5123801715a62e
Rostyk Chaikivskyi <r.chaikivskyi@gmail.com>
2023-05-01 19:08:31 +0300

    ProjectCard

--- app/(dashboard)/home/page.tsx
+++ app/(dashboard)/home/page.tsx
@@ -7,8 +7,26 @@ import { Suspense } from 'react';
 import Link from 'next/link';
 import Greetings from '@/components/Greetings';
 import GreetingsWrapper from '@/components/GreetingsWrapper';
+import ProjectCard from '@/components/ProjectCard';
+
+const getData = async () => {
+  await delay(2000);
+  const user = await getUserFromCookie(cookies() as any);
+  const projects = await db.project.findMany({
+    where: {
+      ownerId: user?.id,
+    },
+    include: {
+      tasks: true,
+    },
+  });
+
+  return { projects };
+};
 
 export default async function Home() {
+  const { projects } = await getData();
+
   return (
     <div className="h-full overflow-y-auto pr-6 w-1/1">
       <div className=" h-full  items-stretch justify-center min-h-[content]">
@@ -18,10 +36,19 @@ export default async function Home() {
             <Greetings />
           </Suspense>
         </div>
+
         <div className="flex flex-2 grow items-center flex-wrap mt-3 -m-3 ">
-          {/** projects map here */}
+          {projects.map(project => (
+            <div className="w-1/3 p-3" key={project.id}>
+              <Link href={`/project/${project.id}`}>
+                <ProjectCard project={project} />
+              </Link>
+            </div>
+          ))}
+
           <div className="w-1/3 p-3">{/* new project here */}</div>
         </div>
+
         <div className="mt-6 flex-2 grow w-full flex">
           <div className="w-full">{/* tasks here */}</div>
         </div>
--- components/GreetingsWrapper.tsx
+++ components/GreetingsWrapper.tsx
@@ -5,7 +5,7 @@ const GreetingsWrapper = () => {
     <Card className="w-full pt-8 pb-4">
       <div className="animate-pulse flex space-x-4">
         <div className="flex-1 space-y-4">
-          <div className="grid grid-cols-5">
+          <div className="grid grid-cols-6">
             <div className="h-9 bg-gray-300 rounded"></div>
           </div>
 
@@ -13,7 +13,7 @@ const GreetingsWrapper = () => {
             <div className="h-7 bg-gray-300 rounded"></div>
           </div>
 
-          <div className="grid grid-cols-4">
+          <div className="grid grid-cols-5">
             <div className="h-14 bg-gray-300 rounded"></div>
           </div>
         </div>
